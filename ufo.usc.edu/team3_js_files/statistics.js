71ffad59341d04836e16763bb49087c910075ff6
Pavneet Kaur <kaurpavneet2410@gmail.com>
2018-04-24 15:43:01 -0700

    Bar-chart visualization
    Signed-off-by: Pavneet Kaur <kaurpavneet2410@gmail.com>

--- ufo.usc.edu/team3_js_files/statistics.js
+++ ufo.usc.edu/team3_js_files/statistics.js
@@ -1,58 +1,182 @@
 var client = new $.es.Client({
-  hosts: 'localhost:9200'
+ hosts: 'localhost:9200'
 });
 
 $(function(){
 
-  client.search({
-    "body":{
-      "aggs":{
-        "types_count":{
-          "terms": {"field": "state.keyword", "size":53}
-        }
-      }
-    }
+ client.search({
+   "body":{
+     "aggs":{
+       "types_count":{
+         "terms": {"field": "state.keyword", "size":53}
+       }
+     }
+   }
+ })
+ .then(function(body){
+   var stateCount = body.aggregations.types_count.buckets;
+
+   var margin = {top: 40, right: 20, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var formatPercent = d3.format(".0%");
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .tickFormat(formatPercent);
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([-10, 0])
+  .html(function(d) {
+    return "<strong>Document Count:</strong> <span style='color:red'>" + d.doc_count + "</span>";
   })
-  .then(function(body){
-    var stateCount = body.aggregations.types_count.buckets;
 
-    var svg = d3.select("body").append("svg").attr("width", "960").attr("height", "960"),
-    margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = +svg.attr("width") - margin.left - margin.right,
-    height = +svg.attr("height") - margin.top - margin.bottom;
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-    var x = d3.scaleBand().rangeRound([0, width]).paddingInner(0.1),
-        y = d3.scaleLinear().rangeRound([height, 0]);
+    svg.call(tip);
 
-    var g = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  function abbrState(input, to){
 
-    x.domain(stateCount.map(function(d) { return d.key; }));
-    y.domain([0, d3.max(stateCount, function(d) { return d.doc_count; })]);
+  var states = [
+      ['Arizona', 'AZ'],
+      ['Alabama', 'AL'],
+      ['Alaska', 'AK'],
+      ['Arizona', 'AZ'],
+      ['Arkansas', 'AR'],
+      ['California', 'CA'],
+      ['Colorado', 'CO'],
+      ['Connecticut', 'CT'],
+      ['Delaware', 'DE'],
+      ['Florida', 'FL'],
+      ['Georgia', 'GA'],
+      ['Hawaii', 'HI'],
+      ['Idaho', 'ID'],
+      ['Illinois', 'IL'],
+      ['Indiana', 'IN'],
+      ['Iowa', 'IA'],
+      ['Kansas', 'KS'],
+      ['Kentucky', 'KY'],
+      ['Kentucky', 'KY'],
+      ['Louisiana', 'LA'],
+      ['Maine', 'ME'],
+      ['Maryland', 'MD'],
+      ['Massachusetts', 'MA'],
+      ['Michigan', 'MI'],
+      ['Minnesota', 'MN'],
+      ['Mississippi', 'MS'],
+      ['Missouri', 'MO'],
+      ['Montana', 'MT'],
+      ['Nebraska', 'NE'],
+      ['Nevada', 'NV'],
+      ['New Hampshire', 'NH'],
+      ['New Jersey', 'NJ'],
+      ['New Mexico', 'NM'],
+      ['New York', 'NY'],
+      ['North Carolina', 'NC'],
+      ['North Dakota', 'ND'],
+      ['Ohio', 'OH'],
+      ['Oklahoma', 'OK'],
+      ['Oregon', 'OR'],
+      ['Pennsylvania', 'PA'],
+      ['Rhode Island', 'RI'],
+      ['South Carolina', 'SC'],
+      ['South Dakota', 'SD'],
+      ['Tennessee', 'TN'],
+      ['Texas', 'TX'],
+      ['Utah', 'UT'],
+      ['Vermont', 'VT'],
+      ['Virginia', 'VA'],
+      ['Washington', 'WA'],
+      ['West Virginia', 'WV'],
+      ['Wisconsin', 'WI'],
+      ['Wyoming', 'WY'],
+  ];
 
-    g.append("g")
-      .attr("class", "axis axis--x")
+  if (to == 'abbr'){
+      input = input.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+      for(i = 0; i < states.length; i++){
+          if(states[i][0] == input){
+              return(states[i][1]);
+          }
+      }
+  } else if (to == 'name'){
+      input = input.toUpperCase();
+      for(i = 0; i < states.length; i++){
+          if(states[i][1] == input){
+              return(states[i][0]);
+          }
+      }
+  }
+}
+
+
+  x.domain(stateCount.map(function(d) {
+    state_ori_name = d.key
+    state_abbr = abbrState(state_ori_name, 'abbr');
+    d['abbr'] = state_abbr
+    return d.abbr; }));
+  y.domain([0, d3.max(stateCount, function(d) { return d.doc_count; })]);
+
+
+
+
+
+
+//
+// d3.tsv("data.tsv", type, function(error, data) {
+//   x.domain(data.map(function(d) { return d.letter; }));
+//   y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+  svg.append("g")
+      .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+      .call(xAxis);
 
-    g.append("g")
-      .attr("class", "axis axis--y")
-      .call(d3.axisLeft(y).ticks(10, "%"))
+  svg.append("g")
+      .attr("class", "y axis")
+      .call(yAxis)
     .append("text")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
-      .attr("dy", "0.71em")
-      .attr("text-anchor", "end")
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
       .text("Frequency");
 
-      g.selectAll(".bar")
+  svg.selectAll(".bar")
       .data(stateCount)
-      .enter().append("rect")
-        .attr("class", "bar")
-        .attr("x", function(d) { return x(d.key); })
-        .attr("y", function(d) { return y(d.doc_count); })
-        .attr("width", x.bandwidth())
-        .attr("height", function(d) { return height - y(d.doc_count); });
-  })
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.abbr); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.doc_count); })
+      .attr("height", function(d) { return height - y(d.doc_count); })
+      .on('mouseover', tip.show)
+      .on('mouseout', tip.hide)
+
+});
+
+function type(d) {
+  d.doc_count = +d.doc_count;
+  return d;
+}
+
+
 
 })
