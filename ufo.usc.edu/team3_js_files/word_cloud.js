d43aad965daf8799887db562120e1eb18fb9284d
Sachin GB <gumblapu@usc.edu>
2018-04-26 00:31:49 -0700

    Reducing word cloud file and fixing ui issues

--- ufo.usc.edu/team3_js_files/word_cloud.js
+++ ufo.usc.edu/team3_js_files/word_cloud.js
@@ -1,6 +1,6 @@
 $(function(){
   queue()
-  .defer(d3.csv, '../data_files/uk_word_frequency.csv')
+  .defer(d3.csv, '../data_files/uk_word_reduced_freq.csv')
   .await(handleData);
 
 function handleData(error, data){
@@ -10,23 +10,23 @@ function handleData(error, data){
     d3.layout.cloud().size([1000, 500])
             .words(data)
             .rotate(0)
-            .fontSize(function(d) { return d.count; })
+            .fontSize(function(d) { return (d.count/16); })
             .on("end", draw)
             .start();
     function draw(words) {
         d3.select("body").append("svg")
                 .attr("width", 1160)
-                .attr("height", 600)
+                .attr("height", 1160)
                 .attr("class", "wordcloud")
                 .append("g")
                 // without the transform, words words would get cutoff to the left and top, they would
                 // appear outside of the SVG area
-                .attr("transform", "translate(320,200)")
+                .attr("transform", "translate(420,360)")
                 .selectAll("text")
                 .data(words)
                 .enter().append("text")
                 .style("font-size", function(d) {
-                  return d.count + "px"; })
+                  return (d.count/16) + "px"; })
                 .style("fill", function(d, i) { return color(i); })
                 .attr("transform", function(d) {
                     return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
